144cb93c719f893ec7f5476a8f3c7e7f5c5f3a03
tusharpal89 <148934774+tusharpal89@users.noreply.github.com>
2025-09-29 16:33:15 +0000

    Comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+
+// console.log("2" > 1) => true
+
+// console.log("02" > 1) => true 
+
+// jab bhi aap comparision kre toh yeh confirm rkhe ki dataType same ho !!
+
+
+
+
+//****** Unpredictable Behaviour****
+
+// console.log(" null > 0") => false
+
+//  console.log("null == 0 ") => false
+
+// console.log("null >= 0 ")  => true
+
+// ********* strict check *****
+
+// ===
+
+
