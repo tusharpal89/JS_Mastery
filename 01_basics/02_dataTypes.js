835312798dbd64a5ca54c1bfcd6fab9c507cc050
tusharpal89 <148934774+tusharpal89@users.noreply.github.com>
2025-09-29 14:58:24 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version !!
+
+// alert(3 + 3) // we are using nodejs, not browser !!
+
+
+
+
+// Data Types 
+
+// number
+// bigint
+// string => ""
+// boolean => true / false
+
+//  null => standalone value hai , jese server k pss temp. value nhi hai toh woh 0 nhi bhejega kyo 0 bhi ek temperatue hota hai, uss case m null bhejna hota hai !!
+
+// undefined => 
+
+// symbol => unique 
+
+// object 
+
+// console.log(typeof age);
+
+// console.log(typeof undefined); --> undefined
+
+// console.log(typeof null); --> object 
+
