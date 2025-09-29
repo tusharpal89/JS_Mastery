954bd74b896bb5dcd4fcadfe1af732c19f3eb9e2
tusharpal89 <148934774+tusharpal89@users.noreply.github.com>
2025-09-29 16:18:17 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33abc";  // 6th line m yeh toh number m convert nhi hona chayiye tha
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueIntNumber = Number(score)
-console.log(typeof valueIntNumber)  // number
-console.log(valueIntNumber)  // --> NaN
+//console.log(typeof valueIntNumber)  // number
+//console.log(valueIntNumber)  // --> NaN
 
 // "33" => 33
 // "33abc" => NaN
@@ -13,8 +13,8 @@ console.log(valueIntNumber)  // --> NaN
 
 let booleanisLoggedIn = "hitesh"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+let booleanIsLoggedIn = Boolean(booleanisLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 // summary
 
@@ -22,3 +22,31 @@ console.log(booleanIsLoggedIn)
 // "" => false
 // "hitesh" => true 
 
+
+
+// *********** Operation *************
+
+let value = 3
+
+let negValue = -value
+
+//console.log(negValue)
+
+  let str1 = "hello "
+
+  let str2 = "Tushar"
+
+  let str3 = str1 + str2
+
+  //console.log(str3)
+
+// console.log ("1" + 2 + 2); // => 122
+
+// console.log( 1 + 2 + "2")  // => 32
+
+// console.log(+true) ==> 1
+
+// console.log(+"") ==> 0
+
+// Link to document : 
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
