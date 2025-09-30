c16c0005fb9b19720468c3c7a1e962d6d9aa9655
tusharpal89 <148934774+tusharpal89@users.noreply.github.com>
2025-09-30 13:13:24 +0000

    Strings_Basics

---
+++ 01_basics/05_String.js
@@ -0,0 +1,46 @@
+  const name = "Tushar "
+
+  const count = 3
+
+  //console.log(name + "count is:" + count) : use backticks wala method Only!!
+
+
+  // ********* Always use Backticks ( String Interpolation) wala Method ***************
+
+  // console.log(`Hello my name is ${name} and my repo count is ${count}`)
+
+  const gameName = new String('hiteshhc');
+ 
+  /*
+  console.log(gameName[0]);
+  console.log(gameName.__proto__);
+
+  */
+
+  // Original String m change nhi aata hai, yeh wala concept aap stack or 
+  // heap se yaad  kr skte hai !!
+ // console.log(gameName.length)
+
+
+
+/*
+  console.log(gameName.charAt(2)) 
+  console.log(gameName.indexOf('t'))  
+
+  */
+
+// ***** Trim => starting k aur end k spaces ko remove kr deta hai !!
+
+// const url = "https://hitesh.com/hitesh%20choudhary";
+
+// console.log(url.replace('%20', '-'));
+
+const name2 = "tushar-pal-arena-Login-in"
+
+console.log(name2.split('-'));
+
+
+
+
+
+  
