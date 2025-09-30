813f048ca9a4270842b4dacf27f691fb45c84014
tusharpal89 <148934774+tusharpal89@users.noreply.github.com>
2025-09-30 13:51:13 +0000

    Math_&_Num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,71 @@
+ /*
+ 
+ const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+
+console.log(otherNumber.toPrecision(4)); // ==> 123.9 
+
+
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+*/
+
+
+
+
+// ++++++++++++ Maths ++++++++++++++++++++++++++
+
+/*
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+
+*/
+
+
+// ************** Math.random() **************
+// Math.random() => ki value hmesha 0 aur 1 k bich m aayegi, 0 bhi ho skti hai aur 1 bhi ho skti hai !!
+
+
+
+
+
+
+// Generates a random decimal number between 0 (inclusive) and 1 (exclusive)
+// console.log(Math.random());
+
+// Generates a random decimal number between 1 and 11 (but never 11)
+// console.log((Math.random() * 10) + 1);
+
+// Generates a random integer between 1 and 10
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+// Define the minimum value of the desired range
+// const min = 10;
+// Define the maximum value of the desired range
+// const max = 20;
+
+// Generates a random integer between min (10) and max (20), inclusive
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
+
+
+
