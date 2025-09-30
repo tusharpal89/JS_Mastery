9acf6021834c3fc6c3415b6cfc3bcaa3e886f075
tusharpal89 <148934774+tusharpal89@users.noreply.github.com>
2025-09-30 09:47:40 +0000

    DataType-Summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,41 @@
+// 1.Primitive
+
+// String, Number, Boolean, null, undefined, symbol, BigInt
+
+
+// ***Js Mastery -> Bss Objects & Browser k events ko master kr Lijiye***
+
+// 2. Non-Primitive / Reference  
+
+// Array, Objects, Functions 
+
+// JS is Dynamically typed Prog. Language !!
+
+const id = Symbol('123')
+
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+ // curly braces k andar jo bhi hai, woh ek Object hai !!
+ 
+ /*
+
+ let myObj = {
+    name : "Tushar",
+    age: 20,
+ }
+
+ */
+
+
+// ****** JS m aap function k bhi variable ki tarah Treat kr skte hai ***
+
+ // const myfunction = function (){}
+
+ 
+
+
+
+
+
