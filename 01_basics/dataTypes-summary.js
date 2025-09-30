ba54a55f4453d337e4be5afefbe3acb480264788
tusharpal89 <148934774+tusharpal89@users.noreply.github.com>
2025-09-30 11:08:52 +0000

    Stack_&_Heap

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -15,7 +15,7 @@ const id = Symbol('123')
 
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
  // curly braces k andar jo bhi hai, woh ek Object hai !!
  
@@ -34,8 +34,42 @@ console.log(id === anotherId);
  // const myfunction = function (){}
 
  
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// +++++++++ Memory ++++++++++++++++++++++++++++++++++++++
 
 
+// Stack ( Primitive) => stack k andar se hmesha ek copy hi milta hai
+// Heap  (Non- Primitive ) => Heap k andar se hmesha ek Reference milta hai
+
+// *** Reference se matlab h jo bhi aap changes krte hai toh 
+// actual values k andar hi krte hai !!
+
+let myYoutubename = "tusharPaldotcom"
+
+let anothername = myYoutubename
+
+anothername = "chaiaurcode"
+
+//console.log(myYoutubename)
+
+//console.log(anothername)
+
+
+// Heap k andar jo bhi jata hai uska Reference hi milta h hmesha !!
+
+let userOne = {
+   email: "tushh@gmail.com",
+   upi : "user@ybl",
+}
+
+let userTwo = userOne
+
+userTwo.email = "tushh@gmail.com"
+
+//console.log(userOne)
+
+//console.log(userTwo)
 
 
 
