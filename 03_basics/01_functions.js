e22b190d352c19cb501003c1cc4638912af65e5d
tusharpal89 <148934774+tusharpal89@users.noreply.github.com>
2025-10-03 17:47:04 +0000

    Functions_Intro

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,61 @@
+// Function = Reusable block of code that performs a specific task.
+
+// function addTwoNumbers(num1, num2){
+
+//     console.log(num1+num2)
+
+// }
+
+// addTwoNumbers(3,4)
+
+// function addTwoNumbers(num1, num2){
+
+//     console.log(num1+num2)
+
+// }
+
+// addTwoNumbers(3,'4')    // 34 
+
+
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+
+// const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);-> Result: undefined, kyoki value return hi nhi ki !!!!
+
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Thda sa Logical Code !!
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("hitesh"))
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Default value of a Parameter 
+// function loginUserMessage(username = "sam"){
+//     if(!username){
+//         console.log("PLease enter a username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+
+// // console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
