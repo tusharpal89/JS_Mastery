ac29e790395bcfd9ad66e88bef3a178b1b1eddc1
tusharpal89 <148934774+tusharpal89@users.noreply.github.com>
2025-10-04 17:22:04 +0000

    Hoisting_and_TDZ

---
+++ 03_basics/02_Scope_IInd_Part.js
@@ -0,0 +1,27 @@
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+       // console.log(username + website);
+    }
+   // console.log(website); --> Error - 01
+}
+
+//console.log(username);  --> Error - 02
+
+
+
+// const addTwo = function(num){   // ---> Function Expression !! 
+//     return num + 2
+// }
+
+// addTwo(5)
+
+addTwo(5) 
+const addTwo = function(num){   // Hoisting and TDZ !!!
+    return num + 2
+}
+
+
+
+
