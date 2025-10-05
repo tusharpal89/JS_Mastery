746381293c3e6a694492cda61a107cad948d7ef8
tusharpal89 <148934774+tusharpal89@users.noreply.github.com>
2025-10-05 19:00:38 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,35 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+// function chai(){
+//     console.log(`DB CONNECTED`);
+// }
+
+// chai()
+
+
+//+++++++++++++++++ IIFE --> ()() ++++++++++++++++++++++++++++++++++++++
+
+// (function chai(){
+//     console.log(`DB CONNECTED`);
+// })
+// chai()
+
+
+// Global Scope k declaration k pollution ko htane k liye humne 
+// IIFE ka use kiya !!
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// IIFE m hmesha semicolon lgana zruri hai end m !!!
+
+// (function chai(){
+//     console.log(`DB CONNECTED`);
+// })
+// chai()
+
+
+// ( (name) => {
+//     console.log(`DB CONNECTED TWO ${name}`);
+// } )('hitesh')
+
+
+
