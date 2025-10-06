2cc080c34517c72e63179fcff540963dc4cb3807
tusharpal89 <148934774+tusharpal89@users.noreply.github.com>
2025-10-06 08:56:54 +0000

    IIFE_03

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -8,7 +8,7 @@
 
 
 //+++++++++++++++++ IIFE --> ()() ++++++++++++++++++++++++++++++++++++++
-
+// first () === for Function Definition &  Second....() === is for Excution
 // (function chai(){
 //     console.log(`DB CONNECTED`);
 // })
@@ -30,6 +30,23 @@
 // ( (name) => {
 //     console.log(`DB CONNECTED TWO ${name}`);
 // } )('hitesh')
+
+
+// An IIFE (Immediately Invoked Function Expression) is a JavaScript function that is executed instantly upon its definition, creating a private scope for variables to prevent the pollution of the global scope.
+
+
+//+++++++++++++++++++++ Named IIFE V/s  Without Name IIFE ++++++++++++++++++
+
+// (function chai(){
+//   // named IIFE
+//   console.log(`DB CONNECTED`);
+// })();
+
+// ( (name) => {
+//   console.log(`DB CONNECTED TWO ${name}`);
+// } )('hitesh')
+ 
+
 
 
 
