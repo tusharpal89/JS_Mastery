f73e10362e75c653c5f3c4f823564d655cfa4d75
tusharpal89 <148934774+tusharpal89@users.noreply.github.com>
2025-10-06 12:34:31 +0000

    Switch_Case

---
+++ 04_Control_flow/Switch.js
@@ -0,0 +1,15 @@
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+
+    case "feb":
+        console.log("feb");
+        break;
+        
+    case "march":
+        console.log("march")
+        break;
+}
