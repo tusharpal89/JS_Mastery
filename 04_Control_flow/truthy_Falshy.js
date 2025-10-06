f96514ec3f2ac1ee5f24855bf37502e568885daa
tusharpal89 <148934774+tusharpal89@users.noreply.github.com>
2025-10-06 13:09:05 +0000

    truthy_Falsy

---
+++ 04_Control_flow/truthy_Falshy.js
@@ -0,0 +1,57 @@
+// Truthy & Flasy --> matlab maan liya gya hai
+// String empty hai toh --> False
+// String hai toh --> True !!
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Empty string --> ko false maan liya !!!
+// Empty Array  --> ko True maan liya !!  (Kya koi Rule hai bhi ya nhi )
+
+
+// const userEmail = "h@hitesh.ai"
+
+// if (userEmail) {
+//     console.log("Got user email");
+// } else {
+//     console.log("Don't have user email");
+// }
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// Inke alawa sb truthy values hai !!
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+
+//*********************************************************************** */
+// Nullish Coalescing Operator (??): null undefined
+
+// let val1;
+// val1 = null ?? 10  // yeh prog. ko crash hone se bachane k liye h, agar kahi se null value aa rhi hai lekin null prog. ko crash kr degi uss case
+// m iss ` Nullish Coalescing Operator (??) ` ka use krte hai !!
+
+// console.log(val1);
+
+
+
+
+//*************************************************************************
+
+// Terniary Operator
+// condition ? true : false
+// const iceTeaPrice = 100
+// iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
+
+//#########################################################################
+
+
+
+
+ 
+
