c4a92ce7b4cedfcdd2a7af086c09af52964de035
tusharpal89 <148934774+tusharpal89@users.noreply.github.com>
2025-10-06 14:45:42 +0000

    Loops_01

---
+++ 05_iterations/one.js
@@ -0,0 +1,27 @@
+    // For Loop !
+    // for(let i=0; i<10; i++){
+    //     const elem = i;
+    //     console.log(elem);
+        
+    // }
+
+
+    // console.log(elem)
+
+
+//     for (let i = 0; i <= 10; i++) {
+//     console.log(`Outer loop value: ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`Inner loop value ${j} and inner loop ${i}`);
+//     }
+// }
+
+
+
+let myArray = ["flash", "batman", "superman"]
+console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+    
+}
