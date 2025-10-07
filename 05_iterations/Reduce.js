90540cf8948158176460d73ab37ed00a1d378b61
tusharpal89 <148934774+tusharpal89@users.noreply.github.com>
2025-10-07 08:14:37 +0000

    Reduce

---
+++ 05_iterations/Reduce.js
@@ -0,0 +1,18 @@
+// Reduce Using `Normal Function`
+
+// const myNums = [1, 2, 3]
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+// console.log(myTotal);
+
+
+//*********************************************************************************************************
+// Reduce using ` Arrow function` 
+
+// const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+// console.log(myTotal);
+
+//#######################################################################################################
+
